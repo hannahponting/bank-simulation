0e66e9da32a5ed747d6eece6cc6377a6d461ba5c
rui jing <jingrui5@163.com>
2023-10-02 15:43:59 +0100

    add ui interface

--- src/main/java/com/example/banksimulation/HelloController.java
+++ src/main/java/com/example/banksimulation/HelloController.java
@@ -2,14 +2,36 @@ package com.example.banksimulation;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField accountNumberInput;
+    @FXML
+    private Label allAccountDetails;
+
+    Bank bank;
+
+    HelloController(Bank bank){
+        this.bank= bank;
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void getAccountDetails(){
+        String output = "";
+        int requestedAccountNumber = Integer.parseInt(accountNumberInput.getText());
+        Account requestedAccount= bank.accountBookHashMap.get(requestedAccountNumber);
+        output += requestedAccount.accountBalance;
+        allAccountDetails.setText(output);
+
     }
 
 
