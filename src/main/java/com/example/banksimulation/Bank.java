e627eb12f0b7d2e9dc0db5ee140a6ebb783ea8f3
Maria O Mullony <mschzomullony@gmail.com>
2023-10-02 13:17:56 +0100

    ReadCSV 1

--- src/main/java/com/example/banksimulation/Bank.java
+++ src/main/java/com/example/banksimulation/Bank.java
@@ -1,10 +1,17 @@
 package com.example.banksimulation;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Bank {
     HashMap<Integer,Account> accountBookHashMap = new HashMap<>();
     HashMap<String,Customer> customerHashMap = new HashMap<>();
+
+    FileInputStream fis;
+    Scanner fileScanner;
 
     //todo: write method to read in a csv of existing accounts and populate the HashMaps
 
@@ -24,11 +31,29 @@ public class Bank {
         accountBookHashMap.put(account.getAccountNumber(),account);
         accountHolder.accountArrayList.add(account);
     }
-//    public void createAccount(double balance, String accountNameHolder) {
-//
-//        Account account = new Account(balance);
-//        account.setAccountHolderName(accountNameHolder);
-//        accountMap.put(account.getAccountNumber(), account);
-//        System.out.println("Account created");
+
+    public void readCSVbankbook (){
+        try {
+            fis = new FileInputStream("src/main/resources");
+            fileScanner = new Scanner (fis);
+            while (fileScanner.hasNextLine()){
+
+            }
+            fis.close();
+
+        }
+        catch (FileNotFoundException fileNotFoundException){
+            System.out.println("Placeholder");
+        }
+        catch (IOException ae){
+            throw new RuntimeException();
+        }
+
+    }
+
+
+
+
+
 }
 
