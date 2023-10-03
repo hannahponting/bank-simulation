44cc15034ebd1765c0e029dadebfc575d0016563
rui jing <jingrui5@163.com>
2023-10-03 16:09:57 +0100

    add login in interface

--- src/main/java/com/example/banksimulation/HelloApplication.java
+++ src/main/java/com/example/banksimulation/HelloApplication.java
@@ -10,16 +10,27 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Bank barclays = new Bank();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ViewAccountDetails.fxml"));
-        HelloController controller = new HelloController();
-        fxmlLoader.setController(controller);
-        controller.setBank(barclays);
-        barclays.readCSVBankAndCustomerBook();
-        Scene scene = new Scene(fxmlLoader.load(), 500, 600);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader fxmlLoaderLogin = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene sceneLogin = new Scene(fxmlLoaderLogin.load(), 520, 340);
+        stage.setTitle("Login page");
+        stage.setScene(sceneLogin);
         stage.show();
+        Bank barclays = new Bank();
+        LoginController loginController = fxmlLoaderLogin.getController();
+        loginController.setBank(barclays);
+
+
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ViewAccountDetails.fxml"));
+//        HelloController controller = new HelloController();
+//        fxmlLoader.setController(controller);
+//        controller.setBank(barclays);
+//        barclays.readCSVBankAndCustomerBook();
+//        Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+
+
 
 
     }
