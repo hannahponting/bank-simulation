e53f499bbb91f83adbaf051c99c4a40fd4fa4f80
Edmund Woodfield <edmundwoodfield@gmail.com>
2023-10-03 10:13:43 +0100

    Sets controller within Application

--- src/main/java/com/example/banksimulation/HelloApplication.java
+++ src/main/java/com/example/banksimulation/HelloApplication.java
@@ -12,12 +12,18 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         Bank barclays = new Bank();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ViewAccountDetails.fxml"));
-        HelloController controller = fxmlLoader.getController();
+        HelloController controller = new HelloController();
+        fxmlLoader.setController(controller);
         controller.setBank(barclays);
+        Customer firstCustomer = barclays.createCustomer("Joe Bloggs");
+        barclays.createAccount(firstCustomer,"current");
+        Customer secondCustomer = barclays.createCustomer("Jane Doe");
+        barclays.createAccount(secondCustomer, "current");
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
 
     }
 
