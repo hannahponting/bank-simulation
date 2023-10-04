a04f270aad87627f44de196a9a39c2bf72410b4d
Maria O Mullony <mschzomullony@gmail.com>
2023-10-04 11:54:48 +0100

    CD Scene Buidler 1

--- src/main/java/com/example/banksimulation/CdCreateController.java
+++ src/main/java/com/example/banksimulation/CdCreateController.java
@@ -1,6 +1,12 @@
 package com.example.banksimulation;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CdCreateController {
+    @FXML
+    private TextField initialInvetsmentInput;
+
 
     private Bank bank;
 
