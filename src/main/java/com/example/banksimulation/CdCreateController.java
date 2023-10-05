b67de191c717a8165f8a299582953bbc69849874
Hannah Ponting <hannahponting01@gmail.com>
2023-10-05 14:09:03 +0100

    changes to cd account

--- src/main/java/com/example/banksimulation/CdCreateController.java
+++ src/main/java/com/example/banksimulation/CdCreateController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Paint;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -58,16 +59,32 @@ public class CdCreateController {
     //TODO: Do something in case no value is inputted and check is pressed
 
     public void addCDAccountToCSV(ActionEvent event) throws IOException {
-        double accountBalance = Double.parseDouble(initialInvetsmentInput.getText());
-        bank.createNewCdAccount(currentCustomer,cdLength,interestrateWeSet,accountBalance);
-        printCongratulationMessagebutton.setText("done");
-        loginController.onHelloButtonClick();
-
-
+        try {
+            double amount = Double.parseDouble(initialInvetsmentInput.getText());
+            if (cdLength == 0) {
+                printCongratulationMessagebutton.setText("You need to choose an account term");
+                printCongratulationMessagebutton.setTextFill(Paint.valueOf("red"));
+            } else {
+                if (amount>0){
+                    double accountBalance = Double.parseDouble(initialInvetsmentInput.getText());
+                    bank.createNewCdAccount(currentCustomer, cdLength, interestrateWeSet, accountBalance);
+                    printCongratulationMessagebutton.setText("Your account has been created");
+                    printCongratulationMessagebutton.setTextFill(Paint.valueOf("black"));
+                    loginController.onHelloButtonClick();
+                }
+                else{
+                    printCongratulationMessagebutton.setText("Amount can't be negative");
+                    printCongratulationMessagebutton.setTextFill(Paint.valueOf("red"));
+                }
+            }
 
 //        cdAccount.interestRateFromCSV = cdAccount.getInterestRate().get(cdLength-1);
 //        cdAccount.accountTerm = (cdLength-1);
 
+        } catch (NumberFormatException nfe){
+            printCongratulationMessagebutton.setText("Invalid input");
+            printCongratulationMessagebutton.setTextFill(Paint.valueOf("red"));
+        }
 
     }
 
