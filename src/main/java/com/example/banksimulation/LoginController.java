a03803eaf6ea0841c9c295c71a96b1eb9479ffd1
Hannah Ponting <hannahponting01@gmail.com>
2023-10-06 10:01:58 +0100

    create save button and fix account number

--- src/main/java/com/example/banksimulation/LoginController.java
+++ src/main/java/com/example/banksimulation/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.banksimulation;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -72,5 +73,10 @@ public class LoginController {
     private  Bank bank;
     public void setBank(Bank bank) {
         this.bank = bank;
+    }
+
+    public void saveToCSV(ActionEvent actionEvent) {
+        bank.writeCSVBankAndCustomerBook();
+        bank.writeLoanCSV();
     }
 }
