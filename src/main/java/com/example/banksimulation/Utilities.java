eb8e53c1bc5f807bb9298c116c6adf931d9ebf9a
Edmund Woodfield <edmundwoodfield@gmail.com>
2023-11-02 16:45:04 +0000

    parsePositiveDouble method amended to return custom error message when parseDouble fails

--- src/main/java/com/example/banksimulation/Utilities.java
+++ src/main/java/com/example/banksimulation/Utilities.java
@@ -2,11 +2,14 @@ package com.example.banksimulation;
 
 public class Utilities {
 
-    public static double parsePositiveDouble(String input){
-        double output = Double.parseDouble(input);
-        if(output < 0){
+    public static double parsePositiveDouble(String input) {
+        try {
+            double output = Double.parseDouble(input);
+            if (output < 0) {
+                throw new NumberFormatException("Input must be a positive number");
+            } else return output;
+        } catch (NumberFormatException numberFormatException) {
             throw new NumberFormatException("Input must be a positive number");
         }
-        else return output;
     }
 }
